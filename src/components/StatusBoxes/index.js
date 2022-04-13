5e6f4d5282210ec8cddedbd93aae0b4954d82342
srcjcm <48743392+carlosjcuevasm@users.noreply.github.com>
2022-04-13 17:04:49 -0400

    style: formatting

--- src/components/StatusBoxes/index.js
+++ src/components/StatusBoxes/index.js
@@ -1,43 +1,46 @@
-import "./style.css"
+import "./style.css";
 
 function StatusBox(props) {
-  let healthy = props.status.success
-  let message = ""
-  if(healthy){
-    message = <p className={`statusBox__health ${healthy ? "": "--isDown"}`}>Healthy</p>
-  }
-  else {
-    message = 
-    <p className={`statusBox__health ${healthy ? "": "--isDown"}`}>OUTAGE</p> 
+  let healthy = props.status.success;
+  let message = "";
+  if (healthy) {
+    message = (
+      <p className={`statusBox__health ${healthy ? "" : "--isDown"}`}>
+        Healthy
+      </p>
+    );
+  } else {
+    message = (
+      <p className={`statusBox__health ${healthy ? "" : "--isDown"}`}>OUTAGE</p>
+    );
   }
 
   return (
-    
     <div className="statusBox">
-        <div className="statusBox__head">
-          <h1 className="statusBox__title">{props.name}</h1>
-        </div>
-        <div className="statusBox__body">
-          <p className={`statusBox__health ${healthy ? "": "--isDown"}`}>{message}</p>
-          <p className={`statusBox__hostname ${healthy ? "": "--isDown"}`}>{healthy ? props.status.hostname : "403 forbidden"}</p>
-          <p className="statusBox__time">{props.status.time}</p>
-        </div>
-    </div>
-      
-  )
-}
-
-
-function StatusBoxes({status}){
-  return (
-    <div className="statusBoxes">
-      {Object.keys(status).map((key)=>{
-            return (
-              <StatusBox name={key} status={status[key]}/>
-            )
-          })}
+      <div className="statusBox__head">
+        <h1 className="statusBox__title">{props.name}</h1>
+      </div>
+      <div className="statusBox__body">
+        <p className={`statusBox__health ${healthy ? "" : "--isDown"}`}>
+          {message}
+        </p>
+        <p className={`statusBox__hostname ${healthy ? "" : "--isDown"}`}>
+          {healthy ? props.status.hostname : "403 forbidden"}
+        </p>
+        <p className="statusBox__time">{props.status.time}</p>
+      </div>
     </div>
   );
 }
 
-export default StatusBoxes
+function StatusBoxes({ status }) {
+  return (
+    <div className="statusBoxes">
+      {Object.keys(status).map((key) => {
+        return <StatusBox name={key} status={status[key]} />;
+      })}
+    </div>
+  );
+}
+
+export default StatusBoxes;
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,8 +1,8 @@
-import './style.css'
+import "./style.css";
 
-function Table({status}) {
+function Table({ status }) {
   return (
-    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+    <div className="table">
       <table>
         <thead>
           <tr>
@@ -14,21 +14,31 @@ function Table({status}) {
           </tr>
         </thead>
         <tbody>
-          {Object.keys(status).map((key)=>{
+          {Object.keys(status).map((key) => {
             return (
               <tr key={key}>
                 <td>{key}</td>
-                <td>{status[key].success ? <span role="img" aria-label="Healthy">✅</span>: <span role="img" aria-label="Down">❌</span>}</td>
+                <td>
+                  {status[key].success ? (
+                    <span role="img" aria-label="Healthy">
+                      ✅
+                    </span>
+                  ) : (
+                    <span role="img" aria-label="Down">
+                      ❌
+                    </span>
+                  )}
+                </td>
                 <td>{status[key].message}</td>
                 <td>{status[key].hostname}</td>
                 <td>{status[key].time}</td>
               </tr>
-            )
+            );
           })}
         </tbody>
       </table>
     </div>
-  )
+  );
 }
 
 export default Table;
